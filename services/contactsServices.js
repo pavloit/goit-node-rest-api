a4e4321c3eff0da80c8dd5cd32d6c81234707cdd
pavloit <dba.devop@gmail.com>
2024-03-17 17:51:29 +0200

    hw3_mongo

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,82 +1,61 @@
-import fs from "fs/promises"
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+import Contact from "../db/contacts.js";
 
-
-const { randomUUID } = await import('node:crypto');
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const contactsPath = path.join(__dirname, "../db/contacts.json");
-
-async function readContacts() {
-    const data = await fs.readFile(contactsPath, { encoding: "utf-8" });
-    return JSON.parse(data);
+async function listContacts() {
+    const contacts = await Contact.find();
+    return contacts;
 }
 
-function writeContacts(contacts) {
-    return fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
-}
-
-export async function listContacts() {
-    try {
-        const contacts = await readContacts();
-        return contacts;
-    } catch (error) {
-        console.error("Error reading contacts:", error)
+async function getContactById(contactId) {
+    const contact = await Contact.findById(contactId);
+    if (!contact) {
+        throw new Error("Contact not found");
     }
+    return contact;
 }
 
-export async function getContactById(contactId) {
-    try {
-        const contacts = await readContacts();
-        const contact = contacts.find(contact => contact.id === contactId);
-        return contact;
-    } catch (error) {
-        console.error("Error getting contact by ID:", error)
+async function removeContact(contactId) {
+    const contact = await Contact.findByIdAndDelete(contactId);
+    if (!contact) {
+        throw new Error("Contact not found");
     }
+    return contact;
 }
 
-export async function removeContact(contactId) {
-    try {
-        const contacts = await readContacts();
-        const index = contacts.findIndex((contact) => contact.id === contactId);
-        if (index === -1) {
-            return null;
-        }
-        const deletedContact = contacts.splice(index, 1);
-        await writeContacts(contacts);
-        return deletedContact;
+async function addContact({ name, email, phone }) {
+    const newContact = new Contact({ name, email, phone });
+    const result = await newContact.save();
+    return result;
+}
 
-    } catch (error) {
-        console.error("Error removing contact:", error)
+async function updateContact(id, updatedContact) {
+    const contact = await Contact.findByIdAndUpdate(id, updatedContact, {
+        new: true,
+    });
+    if (!contact) {
+        throw new Error("Contact not found");
     }
+    return contact;
 }
 
-export async function addContact({ name, email, phone }) {
-    try {
-        const contacts = await readContacts();
-        const newcontact = { id: randomUUID(), name, email, phone };
-        contacts.push(newcontact);
-        await writeContacts(contacts);
-        return newcontact;
-
-    } catch (error) {
-        console.error("Error adding new contact:", error)
+async function updateStatusContact(id, favorite) {
+    const contact = await Contact.findByIdAndUpdate(
+        id,
+        { favorite },
+        { new: true }
+    );
+    if (!contact) {
+        throw new Error("Contact not found");
     }
+    return contact;
 }
 
-export async function update(id, updatedContact) {
-    try {
-        const contacts = await listContacts();
-        const index = contacts.findIndex((contact) => contact.id === id);
-        if (index === -1) {
-            return null;
-        }
-        contacts[index] = { ...contacts[index], ...updatedContact };
-        await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-        return contacts[index];
-    } catch (error) {
-        console.error("Error updating contact:", error);
-    }
-}
+const contactsService = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+    updateContact,
+    updateStatusContact,
+};
+
+export default contactsService;
